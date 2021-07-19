cf01373b67c6b3fef80ab47a4acc97577b8dc730
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-07-19 19:59:14 +0200

    user fetch app after 39

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterManagement from './components/CounterManagement';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>My App</h1>
+        <CounterManagement ownerName="Richard" />
+      </>
+    )
+  }
 }
 
 export default App;
