6b0db458cf58d17957d991497a2bb828a2b4e471
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-07-31 13:24:50 +0200

    end of lecture

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from '../Profile';
 
 class HomePage extends React.Component {
+  profileRef = React.createRef<Profile>();
+
+  componentDidMount() {
+    console.log(ReactDOM.findDOMNode(this.profileRef.current));
+  }
+
   render() {
     return (
       <div>
         <h1>Home Page</h1>
+        <Profile ref={this.profileRef} />
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -11,19 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('renderSample'),
-  () => {
-    console.log('renderSample rendered');
-  }
-);
-
-const hasUnmountedRenderSampleComponent = ReactDOM.unmountComponentAtNode(document.getElementById('renderSample')!)
-console.log('has unmounted:', hasUnmountedRenderSampleComponent);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
