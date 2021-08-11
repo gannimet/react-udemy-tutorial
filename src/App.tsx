9e6d85a404776a10c22263c34b48fb4523f1a963
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-08-11 17:48:54 +0200

    installed redux and react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import AboutPage from './components/AboutPage';
-import Button from './components/Button';
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
-import LoginContextProvider, { LoginContext, LoginContextType } from './context/LoginContext';
 
 class App extends React.Component {
-  handleLogout = (context: LoginContextType) => {
-    context.setLoginStatus(false);
-  }
-
   render() {
     return (
-      <LoginContextProvider>
-        <LoginContext.Consumer>
-          {(context) => {
-            const { isLoggedIn } = context;
-
-            return (
-              <BrowserRouter>
-                {isLoggedIn && <Header />}
-
-                <Switch>
-                  {isLoggedIn && <Route exact component={HomePage} path="/" />}
-                  {isLoggedIn && <Route component={AboutPage} path="/about" />}
-                  {!isLoggedIn && <Route component={LoginPage} path="/login" />}
-                  <Redirect to={isLoggedIn ? '/' : '/login'} />
-                </Switch>
-
-                {isLoggedIn && <Button onClick={() => this.handleLogout(context)}>Log Out</Button>}
-              </BrowserRouter>
-            )
-          }}
-        </LoginContext.Consumer>
-      </LoginContextProvider>
+      <h1>Welcome to React</h1>
     )
   }
 }
--- src/components/Input/interface.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface InputProps {
-  label: string;
-  onChange(event: React.ChangeEvent<HTMLInputElement>): void;
-  hasError: boolean;
-}
-
-export interface InputState {}
