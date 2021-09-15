d151fface8490b7e86db1559be0fcef5ce64937a
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-09-15 16:21:01 +0200

    end of hands-on

--- src/components/SuperComponent/index.tsx
+++ src/components/SuperComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const expensiveCalculateInitialValue = () => {
   console.log('calcilate initial state');
@@ -7,10 +7,33 @@ const expensiveCalculateInitialValue = () => {
 
 const SuperComponent: React.FC = () => {
   const [counter, setCounter] = useState(() => expensiveCalculateInitialValue());
+  const [otherValue, setOtherValue] = useState(0);
 
   const handleButtonClick = () => {
     setCounter(counter + 1);
   };
+
+  useEffect(() => {
+    console.log('UseEffect triggered');
+  });
+
+  useEffect(() => {
+    console.log('Component did mount');
+  }, []);
+
+  useEffect(() => {
+    console.log('Other value has changed');
+  }, [otherValue]);
+
+  useEffect(() => {
+    console.log('addEventListener');
+    window.addEventListener('click', handleButtonClick);
+
+    return function cleanup() {
+      console.log('removeEventListener');
+      window.removeEventListener('click', handleButtonClick);
+    };
+  });
 
   return (
     <div className="App">
