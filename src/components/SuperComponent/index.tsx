987c03775a7e39d82a958a7709fa388d4949e822
Richard Wotzlaw <r.wotzlaw@gmail.com>
2021-09-17 10:38:06 +0200

    end of hands-on

--- src/components/SuperComponent/index.tsx
+++ src/components/SuperComponent/index.tsx
@@ -1,44 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
-const expensiveCalculateInitialValue = () => {
-  console.log('calcilate initial state');
-  return 0;
-};
-
 const SuperComponent: React.FC = () => {
-  const [counter, setCounter] = useState(() => expensiveCalculateInitialValue());
-  const [otherValue, setOtherValue] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [newValue, setNewValue] = useState(0);
 
   const handleButtonClick = () => {
     setCounter(counter + 1);
   };
 
+  const handleNewValueButtonClick = () => {
+    setNewValue(newValue + 1);
+  };
+
+  /**
+   * Runs AFTER React paints changes in the DOM
+   * Asynchronously, non-blocking
+   */
   useEffect(() => {
     console.log('UseEffect triggered');
-  });
+  }, [counter, newValue]);
 
-  useEffect(() => {
-    console.log('Component did mount');
-  }, []);
-
-  useEffect(() => {
-    console.log('Other value has changed');
-  }, [otherValue]);
-
-  useEffect(() => {
-    console.log('addEventListener');
-    window.addEventListener('click', handleButtonClick);
-
-    return function cleanup() {
-      console.log('removeEventListener');
-      window.removeEventListener('click', handleButtonClick);
-    };
-  });
+  /**
+   * Runs BEFORE React paints changes in the DOM
+   * Synchronously, blocking
+   * Updates scheduled inside will be flushed synchronously
+   */
+  // useLayoutEffect(() => {
+  //   if (counter === 0) {
+  //     setCounter(5);
+  //   }
+  // });
 
   return (
     <div className="App">
       <p>Counter: {counter}</p>
-      <button onClick={handleButtonClick}>Add Value</button>
+      <p>New Value: {newValue}</p>
+      <button onClick={handleButtonClick}>Add Counter</button>
+      <button onClick={handleNewValueButtonClick}>Add New Value</button>
     </div>
   )
 };
